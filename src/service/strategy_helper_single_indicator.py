e584c985c54fce08739029d806ce54f560030a42
zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2019-02-23 12:29:53 -0300

    lots of refactorings and unit tests

--- src/service/strategy_helper_single_indicator.py
+++ src/service/strategy_helper_single_indicator.py
@@ -1,18 +1,22 @@
 import src.service.trade_helper as trade_helper
 
 
-def nao_estou_comprado_logic(indicator, i):
+def have_not_bought_logic(indicator, i):
     return 'COMPRAR' if indicator['series'][i] > indicator['rule']['buy'] else 'NOP'
 
 
-def estou_comprado_logic(indicator, i):
+def have_bought_logic(indicator, i):
     return 'VENDER' if indicator['series'][i] < indicator['rule']['sell'] else 'NOP'
 
 
 def create_operation_for_today(dates, estou_comprado, indicator, i):
-    operation = estou_comprado_logic(indicator, i) if estou_comprado else nao_estou_comprado_logic(indicator, i)
+    operation = have_bought_logic(indicator, i) if estou_comprado else have_not_bought_logic(indicator, i)
 
-    return dates[i], operation
+    return {
+        'date': dates[i],
+        'operation': operation,
+        'indicator_value': indicator['series'][i]
+    }
 
 
 def get_orders(dates, price, indicator):
@@ -20,7 +24,11 @@ def get_orders(dates, price, indicator):
 
     all_operations = []
     for i in range(0, len(dates)):
-        date, operation = create_operation_for_today(dates, estou_comprado, indicator, i)
+        operation_data = create_operation_for_today(dates, estou_comprado, indicator, i)
+
+        date = operation_data['date']
+        operation = operation_data['operation']
+
         estou_comprado = trade_helper.resolve_estou_comprado_flag(estou_comprado, operation)
         all_operations.append((date, operation, price[i]))
 
--- test/strategy_helper_single_indicator_test.py
+++ test/strategy_helper_single_indicator_test.py
@@ -1,5 +1,5 @@
 import unittest
-# import datetime
+import datetime
 import src.service.strategy_helper_single_indicator as strategy_helper_single_indicator
 # from decimal import *
 
@@ -22,7 +22,7 @@ class TestUM(unittest.TestCase):
         i = 2
 
         # when
-        result = strategy_helper_single_indicator.nao_estou_comprado_logic(indicator, i)
+        result = strategy_helper_single_indicator.have_not_bought_logic(indicator, i)
 
         # then
         self.assertEqual(result, 'COMPRAR')
@@ -40,7 +40,7 @@ class TestUM(unittest.TestCase):
         i = 1
 
         # when
-        result = strategy_helper_single_indicator.nao_estou_comprado_logic(indicator, i)
+        result = strategy_helper_single_indicator.have_not_bought_logic(indicator, i)
 
         # then
         self.assertEqual(result, 'NOP')
@@ -58,7 +58,7 @@ class TestUM(unittest.TestCase):
         i = 1
 
         # when
-        result = strategy_helper_single_indicator.estou_comprado_logic(indicator, i)
+        result = strategy_helper_single_indicator.have_bought_logic(indicator, i)
 
         # then
         self.assertEqual(result, 'VENDER')
@@ -76,10 +76,92 @@ class TestUM(unittest.TestCase):
         i = 2
 
         # when
-        result = strategy_helper_single_indicator.estou_comprado_logic(indicator, i)
+        result = strategy_helper_single_indicator.have_bought_logic(indicator, i)
 
         # then
         self.assertEqual(result, 'NOP')
+
+
+    def test_create_operation_when_have_bought(self):
+        # given
+        dates = [
+            datetime.datetime(2010, 10, 10),
+            datetime.datetime(2010, 10, 11),
+            datetime.datetime(2010, 10, 12),
+            datetime.datetime(2010, 10, 13)
+        ]
+
+        estou_comprado = True
+
+        indicator = {
+            'series': [-.5, .5, .3, -.1],
+            'rule': {
+                'buy': 0,
+                'sell': 0
+            }
+        }
+
+        i = 3
+
+        expected = {
+            'date': dates[3],
+            'operation': 'VENDER',
+            'indicator_value': indicator['series'][i]
+        }
+
+        # when
+        result = strategy_helper_single_indicator.create_operation_for_today(dates, estou_comprado, indicator, i)
+
+        self.assertEqual(expected, result)
+
+    def test_create_operation_when_have_not_bought(self):
+        # given
+        dates = [
+            datetime.datetime(2010, 10, 10),
+            datetime.datetime(2010, 10, 11),
+            datetime.datetime(2010, 10, 12),
+            datetime.datetime(2010, 10, 13)
+        ]
+
+        estou_comprado = False
+
+        indicator = {
+            'series': [-.5, .5, -.3, -.1],
+            'rule': {
+                'buy': 0,
+                'sell': 0
+            }
+        }
+
+        i = 2
+
+        expected = {
+            'date': dates[2],
+            'operation': 'NOP',
+            'indicator_value': indicator['series'][i]
+        }
+
+        # when
+        result = strategy_helper_single_indicator.create_operation_for_today(dates, estou_comprado, indicator, i)
+
+        self.assertEqual(expected, result)
+
+    def test_get_orders_when_should_not_force_to_sell_on_the_last_day(self):
+        # given
+        dates = None
+        price = None
+        indicator = None
+
+        expected = None
+
+        # when
+        result = strategy_helper_single_indicator.get_orders(dates, price, indicator)
+
+        # then
+        self.fail()
+
+    # def test_get_orders_when_should_force_to_sell_on_the_last_day(self):
+    #     self.fail()
 
 
 if __name__ == '__main__':
