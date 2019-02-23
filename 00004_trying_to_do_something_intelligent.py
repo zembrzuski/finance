3112feb489a4e0718c8678ebdc4767a7120f7e6b
zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2019-02-23 09:37:26 -0300

    ae

--- 00004_trying_to_do_something_intelligent.py
+++ 00004_trying_to_do_something_intelligent.py
@@ -1,12 +1,11 @@
 import service.file_io_service as file_io_service
 import service.date_helper as date_helper
 import numpy as np
-import strategy.buy_and_hold_strategy as buy_and_hold
+import strategy.buy_and_hold_strategy as buy_and_hold_strategy
 import strategy.macd_strategy as macd_strategy
 import service.trade_helper as trade_helper
 import strategy.rsi_strategy as rsi_strategy
 import strategy.rsi_and_macd_strategy as rsi_and_macd_strategy
-import pprint
 
 
 def main():
@@ -16,7 +15,7 @@ def main():
     close = file_content['Adj Close'].values
     date = np.array(list(map(lambda x: date_helper.parse_date_to_datetime(x), file_content['Date'])))
 
-    trades = buy_and_hold.execute(date, close)
+    trades = buy_and_hold_strategy.execute(date, close)
     profit = list(map(lambda trade: trade_helper.compute_profit(trade), trades))
 
     print('lucro com buy and hold: ' + str(profit[0]['profit_percentage']))
