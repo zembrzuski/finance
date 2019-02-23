d0d6eef785f31a132333e67d1335efb7dec2f260
zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2019-02-23 10:32:34 -0300

    tests.
    now, I am sure my 'compoe_lucros' and 'compute_statistics' are working perfectly'

--- src/service/trade_helper.py
+++ src/service/trade_helper.py
@@ -1,6 +1,7 @@
 import numpy as np
 from decimal import *
 
+
 def compute_profit(a_trade):
     return {
         'trade_time': a_trade['sell']['date'] - a_trade['buy']['date'],
@@ -9,6 +10,11 @@ def compute_profit(a_trade):
 
 
 def compute_statistics_from_orders(all_orders):
+    # all_orders eh uma lista de order
+    # order eh uma tupla como no exemplo: (datetime, 'COMPRAR', Decimal(34.35))
+    # order eh uma tupla como no exemplo: (datetime, 'NOP', Decimal(3.35))
+    # order eh uma tupla como no exemplo: (datetime, 'VENDER', Decimal(1.2))
+
     statistics = []
 
     for i in range(0, len(all_orders), 2):
@@ -27,14 +33,20 @@ def compute_statistics_from_orders(all_orders):
 
 
 def compoe_lucros(statistics):
+    # statistics eh um dict como no exempo
+    # {
+    #   'numero_dias_ordem': DIFERENCA ENTRE DOIS DATETIMES (nao lembro qual eh o tipo do objeto)
+    #   'percentual_lucro': Decimal(32.5)  --> isso no caso do trade ter dado 32.5% de lucro.
+    # }
+
     lucros = np.array(list(map(lambda x: x['percentual_lucro'], statistics)))
 
-    dinheiro = 100
+    dinheiro = Decimal(100.)
 
     for lucro in lucros:
-        dinheiro = (lucro / 100 * dinheiro) + dinheiro
+        dinheiro = (lucro / Decimal(100.) * dinheiro) + dinheiro
 
-    return dinheiro - 100
+    return dinheiro - Decimal(100.)
 
 
 def resolve_estou_comprado_flag(estou_comprado, operation):
--- test/trade_helper_test.py
+++ test/trade_helper_test.py
@@ -1,6 +1,7 @@
 import unittest
 import datetime
 import src.service.trade_helper as trade_helper
+from decimal import *
 
 
 class TestUM(unittest.TestCase):
@@ -35,6 +36,53 @@ class TestUM(unittest.TestCase):
         ]
 
         self.assertEqual(result, expected)
+
+
+    def test_compoe_lucros(self):
+        input = [
+            {
+                'numero_dias_ordem': None,
+                'percentual_lucro': Decimal(20.)
+            },
+            {
+                'numero_dias_ordem': None,
+                'percentual_lucro': Decimal(-6.25)
+            },
+            {
+                'numero_dias_ordem': None,
+                'percentual_lucro': Decimal(-20.3)
+            },
+            {
+                'numero_dias_ordem': None,
+                'percentual_lucro': Decimal(17.8)
+            }
+        ]
+
+        expected = Decimal(5.622425)
+
+        result = trade_helper.compoe_lucros(input)
+
+        self.assertEqual(
+            expected.quantize(Decimal('.000001'), rounding=ROUND_DOWN),
+            result.quantize(Decimal('.000001'), rounding=ROUND_DOWN)
+        )
+
+    def test_compoe_lucros_again(self):
+        input = [
+            {
+                'numero_dias_ordem': None,
+                'percentual_lucro': Decimal(230.)
+            }
+        ]
+
+        expected = Decimal(230.)
+
+        result = trade_helper.compoe_lucros(input)
+
+        self.assertEqual(
+            expected.quantize(Decimal('.000001'), rounding=ROUND_DOWN),
+            result.quantize(Decimal('.000001'), rounding=ROUND_DOWN)
+        )
 
 
 if __name__ == '__main__':
