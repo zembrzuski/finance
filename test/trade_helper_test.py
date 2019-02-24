871b9cac194e5bbaf47497c372bc6ee794b3bc97
rodrigo claro <zembrzuski@gmail.com>
2019-02-24 12:44:41 -0300

    tests fixed

--- test/trade_helper_test.py
+++ test/trade_helper_test.py
@@ -9,43 +9,6 @@ class TestUM(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_compute_statistics_from_orders(self):
-        date_order_1 = datetime.datetime(2010, 10, 10)
-        date_order_2 = datetime.datetime(2010, 10, 12)
-        date_order_3 = datetime.datetime(2010, 12, 10)
-        date_order_4 = datetime.datetime(2011, 12, 10)
-
-        order1 = (date_order_1, 'COMPRAR', 10.)
-        order2 = (date_order_2, 'VENDER', 12.)
-        order3 = (date_order_3, 'COMPRAR', 16.)
-        order4 = (date_order_4, 'VENDER', 15.)
-
-        all_orders = [order1, order2, order3, order4]
-
-        result = trade_helper.compute_statistics_from_orders(all_orders)
-
-        expected = [
-            {
-                'numero_dias_ordem': 2,
-                'percentual_lucro': 20.
-            },
-            {
-                'numero_dias_ordem': 365,
-                'percentual_lucro': -6.25
-            }
-        ]
-
-        self.assertEqual(result, expected)
-
-    def test_compute_statistics_from_empty_orders(self):
-        all_orders = []
-
-        result = trade_helper.compute_statistics_from_orders(all_orders)
-
-        expected = []
-
-        self.assertEqual(result, expected)
-
     def test_compoe_lucros(self):
         input = [
             {
