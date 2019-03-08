f7d999cebc81a8d78a4e6c9f72a9e8917d7f69da
zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2019-03-08 06:37:12 -0300

    url mais magica ainda

--- src/main/00006_real_time_poller.py
+++ src/main/00006_real_time_poller.py
@@ -1,20 +1,13 @@
 import requests
 import re
 
-URL_MAGICA = "https://query1.finance.yahoo.com/v7/finance/quote?formatted=true&crumb=U4e8eDQi%2FyI&lang=en-US&region=US&symbols=GGBR4.SA&fields=messageBoardId%2ClongName%2CshortName%2CmarketCap%2CunderlyingSymbol%2CunderlyingExchangeSymbol%2CheadSymbolAsString%2CregularMarketPrice%2CregularMarketChange%2CregularMarketChangePercent%2CregularMarketVolume%2Cuuid%2CregularMarketOpen%2CfiftyTwoWeekLow%2CfiftyTwoWeekHigh&corsDomain=finance.yahoo.com"
+URL_MAGICA = "https://query1.finance.yahoo.com/v7/finance/quote?formatted=true&crumb=U4e8eDQi%2FyI&" \
+             "lang=en-US&region=US&symbols=" \
+             "{}" \
+             "&fields=messageBoardId%2ClongName%2CshortName%2CmarketCap%2CunderlyingSymbol%2CunderlyingExchangeSymbol%2CheadSymbolAsString%2CregularMarketPrice%2CregularMarketChange%2CregularMarketChangePercent%2CregularMarketVolume%2Cuuid%2CregularMarketOpen%2CfiftyTwoWeekLow%2CfiftyTwoWeekHigh&corsDomain=finance.yahoo.com"
 
 def main():
-    resp_content = requests.get('https://finance.yahoo.com/quote/PETR4.SA?p=PETR4.SA&.tsrc=fin-srch').content.decode('utf-8')
-    match = re.match(r'.*26\.', resp_content)
-
-    re.match(r'.*en-US', resp_content)
-
-    re.match(r'.*26', 'gremio26tricolor26')
-
-    resp_content
-
-    if match:
-        print('aer')
+    resp = requests.get(URL_MAGICA.format('PETR4.SA,GGBR4.SA')).json()
 
     print('oir')
 
