6d4b15a45eed9b19c8ad1acb92a60f06c2ec2456
rodrigo claro <zembrzuski@gmail.com>
2019-05-06 20:02:27 -0300

    ae

--- src/main/00007_data_preparation.py
+++ src/main/00007_data_preparation.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix as confusion
 
 
 def get_labeled_quotes(company_code):
@@ -43,12 +44,40 @@ def add_rsi(historical_data):
     return historical_data.join(rsis_dataframe)
 
 
+def create_lots_of_macds(historical_data):
+    prices = historical_data['Adj Close'].as_matrix()
+
+    all_macds = dict()
+
+    for fast_period in range(9, 16):
+        for slow_period in range(20, 31):
+            for signal_period in range(5, 12):
+                macd, macdsignal2, macdhist = MACD(
+                    prices,
+                    fastperiod=fast_period,
+                    slowperiod=slow_period,
+                    signalperiod=signal_period
+                )
+
+                all_macds['macd_{}_{}_{}'.format(fast_period, slow_period, signal_period)] = macdsignal2
+
+    macds_dataframe = pd.DataFrame(all_macds)
+
+    return historical_data.join(macds_dataframe)
+
+
 def main():
     print('init data preparation')
+
+    # company_code = 'BBAS3.SA'
     company_code = 'PETR4.SA'
 
     historical_data = get_labeled_quotes(company_code)
     historical_data = add_rsi(historical_data)
+    historical_data = create_lots_of_macds(historical_data)
+
+    historical_data = historical_data.dropna()
+    historical_data = historical_data.reset_index(drop=True)
 
     # print(historical_data['label'].value_counts())
     # sns.countplot(x='label', data=historical_data, palette='hls')
@@ -56,10 +85,21 @@ def main():
     # historical_data.groupby('label').mean()
 
     y = historical_data['label']
-    X = historical_data.drop(columns=['label', 'Next_Day_Close'])
+    X = historical_data.drop(columns=['label', 'Next_Day_Close', 'Date', 'Datee', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
+    # X = historical_data.drop(columns=['label', 'Next_Day_Close', 'Date', 'Datee', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
+    logreg = LogisticRegression()
+    logreg.fit(X_train, y_train)
+
+    y_pred = logreg.predict(X_test)
+
+    print('Accuracy of logistic regression classifier on train set: {:.2f}'.format(logreg.score(X_train, y_train)))
+    print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+
+    confusion_matrix = confusion(y_test, y_pred)
+    print(confusion_matrix)
 
     print('finished')
 
